901a714146f82622c5fddb6fd4c2f5f020e0b0b1
DuongKien <ddk0705@gmail.com>
2022-05-03 04:23:37 +0700

    Update actions

--- src/app/states/coincap/coincap.action.ts
+++ src/app/states/coincap/coincap.action.ts
@@ -3,11 +3,6 @@ import { createAction, props } from '@ngrx/store';
 // Interfaces
 import { ICoincapAsset } from '../../interfaces';
 
-export const storeListCoins = createAction(
-  '[Coincap] Store list coins',
-  props<{ data: ICoincapAsset[] }>()
-);
-
 export const fetchListCoins = createAction('[Coincap] Fetch list coins');
 
 export const fetchListCoinsSuccess = createAction(
